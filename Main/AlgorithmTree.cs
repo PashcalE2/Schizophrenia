f1c96c021c50c8a16d0eba90b24a7533c52f719a
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2023-12-06 18:38:48 +0300

    Amogus

---
+++ Main/AlgorithmTree.cs
@@ -0,0 +1,22 @@
+﻿namespace Schizophrenia
+{
+    public abstract class AlgorithmNode
+    {
+
+
+        public AlgorithmNode() 
+        { 
+        }
+
+        public abstract void Run(Context ctx);
+
+        public abstract bool IsNext(Context ctx);
+    }
+
+    public class AlgorithmTree
+    {
+        public AlgorithmTree() 
+        { 
+        }
+    }
+}
